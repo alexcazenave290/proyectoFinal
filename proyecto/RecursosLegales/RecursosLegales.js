ff7406b63039553b34d0b05f20ac82fa813895e9
alexcazenave290 <alexcazenave290@gmail.com>
2025-10-28 19:31:37 -0300

    Delete proyecto directory

--- proyecto/RecursosLegales/RecursosLegales.js
+++
@@ -1,6 +0,0 @@
-// ===== Sidebar hamburguesa =====
-const menuToggle = document.getElementById("menu-toggle");
-const sidebar = document.querySelector(".sidebar");
-menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
